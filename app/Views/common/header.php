959478a55a4c00819a65ae058646f7b1015d6c83
GilmarBastos <maz.bst@gmail.com>
2023-12-05 19:24:34 -0300

    update navbar-dropdown3

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -38,7 +38,7 @@
 
                 <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                    Tecnologias
                 </a>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="https://codeigniter4.github.io/userguide/">CodeIgniter4</a></li>
